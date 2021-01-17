e713ffcd486ad58a522a1e76fe33f7a8dd33ceca
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2021-01-17 17:14:56 +0100

    added game code

--- Backend/app.py
+++ Backend/app.py
@@ -11,39 +11,71 @@ GPIO.setmode(GPIO.BCM)
 
 app = Flask(__name__)
 
+espTopics = ['unit1/output', 'unit2/output']
+espMultiplayerTopics = ['unit1/multiplayer/output', 'unit2/multiplayer/output']
+messageReceived = False
+
 @app.route("/")
 def index():
-    return "hello"
+    return "Hello"
 
-@app.route("/sendmessage")
-def flasksendmessage():
-    sendMessage()
-    return "Sent message."
+@app.route("/start")
+def start():
+    startGame()
+    return "Game ended"
 
-def sendMessage():
-    message = {"status": False, "game_ended": False}
-    client.publish('unit1/multiplayer/output', json.dumps(message))
+def startGame():
+    global messageReceived
+    randomTopic = random.choice(espTopics)
+    sendMessage(True, False, randomTopic)
+    level = 0
+    levelTime = 90
+    print("Game started")
+    while True:
+        if messageReceived == True:
+            sendMessage(False, False, randomTopic)
+            level += 1
+            print(f"level {level} completed")
+            print("Waiting for next level to start..")
+            time.sleep(5)
+            randomTopic = random.choice(espTopics)
+            sendMessage(True, False, randomTopic)
+            levelTime = 90
+            messageReceived = False
+            print(f"level {level + 1} started")
+            continue
+
+        if levelTime == 0:
+            print("level failed!")
+            for topic in espTopics:
+                sendMessage(False, True, topic)
+            break
+
+        print(levelTime)
+        levelTime -= 1
+        time.sleep(1)
+
+def sendMessage(status, game_ended, topic):
+    message = {"status": status, "game_ended": game_ended}
+    client.publish(topic, json.dumps(message))
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
 def on_message(client, userdata, msg):
+    global messageReceived
     print(msg.topic+" "+str(msg.payload))
+    messageReceived = True
     #obj = json.loads(str(msg.payload,"UTF8"))
     #global isFreeParkingEnabled
     #isFreeParkingEnabled = obj['isFreeParkingEnabled']
     #print(isFreeParkingEnabled)
     
 if __name__ == '__main__':
- 
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect("192.168.4.1", 1883, 60)
     client.subscribe("pi/output")
     client.loop_start()
-    sendMessage()
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.4.1', port=5000)
